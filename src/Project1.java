46f56e031899dd930bf411befdbc7b46fd536043
Arlita-pi19-1 <protopopova.aelita@gmail.com>
2020-12-03 18:56:49 +0900

    1

---
+++ src/Project1.java
@@ -0,0 +1,8 @@
+public class Project1 {
+    public static boolean main(int a, int b) {
+        System.out.println("\nЗадание 1.");
+        int sum = a + b;
+        if (sum > 10 && sum < 20) return true;
+        else return false;
+    }
+}
